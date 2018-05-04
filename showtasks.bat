5f282631cff5dd882719f3400d03528feeae604c
Jaroslaw Soja <jarek.soja@gmail.com>
2018-05-04 21:13:00 +0200

    First attempt to Heroku

--- showtasks.bat
+++ showtasks.bat
@@ -12,7 +12,7 @@ echo failed starting Chrome - breaking work
 goto fail
 
 :showtasks
-start http://localhost:8080/crud/v1/task/getTasks
+start http://localhost:8080/crud/v1/task/getTasksheroku login
 if %ERRORLEVEL% == 0 goto end
 echo.
 echo failed loading tasks - breaking work
