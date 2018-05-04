22e0f2399e8a21e58dfbcbc7835d9c3f0faf749f
Jaroslaw Soja <jarek.soja@gmail.com>
2018-05-04 18:31:58 +0200

    Deployment 101, including first scripts

---
+++ showtasks.bat
@@ -0,0 +1,26 @@
+call runcrud.bat
+if %ERRORLEVEL% == 0 goto runchrome
+echo.
+echo runcrud has encountered errors - breaking work
+goto fail
+
+:runchrome
+start chrome http://localhost:8080/crud/v1/task/getTasks
+if %ERRORLEVEL% == 0 goto showtasks
+echo.
+echo failed starting Chrome - breaking work
+goto fail
+
+:showtasks
+start http://localhost:8080/crud/v1/task/getTasks
+if %ERRORLEVEL% == 0 goto end
+echo.
+echo failed loading tasks - breaking work
+goto fail
+
+:fail
+echo.
+echo Errors were found.
+
+:end
+echo Ending work
